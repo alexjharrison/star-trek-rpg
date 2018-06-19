d74fde33de4aec161e75ab4d9b3a144a0abaaf03
Alex Harrison <alexjharrison@gmail.com>
2018-06-19 08:04:23 -0400

    sidebar works and teams get picked

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -11,22 +11,22 @@ var klingon = {
     shieldHP: 160,
     fullName: "Klingon Empire",
     shortName: "klingon",
-    fighter: ["General Martok", "TKuvma", "Chancellor Gowron"],
+    fighter: ["Martok", "TKuvma", "Gowron"],
     ship: ["IKS Rotarran", "The Sarcophagus", "IKS Bortas"]
 }
 var romulan = {
     shieldHP: 120,
     fullName: "Romulan Star Empire",
     shortName: "romulan",
-    fighter: ["Nero", "Sela", "Commander Toreth"],
+    fighter: ["Nero", "Sela", "Toreth"],
     ship: ["Narada", "Enterprise-C", "IRW Khazara"]
 }
 var dominion = {
     shieldHP: 100,
     fullName: "Dominion",
     shortName: "dominion",
-    fighter: ["Female Changeling", "Jem Hadar", "Gul Dukat"],
-    ship: ["The Great Link", "Jem Hadar Fighter", "Dukats Bird-of-Prey"]
+    fighter: ["Changeling", "Jem Hadar", "Gul Dukat"],
+    ship: ["The Great Link", "Jem Hadar Fighter", "Bird-of-Prey"]
 }
 
 var initialScreen = function () {
@@ -50,22 +50,25 @@ var initialScreen = function () {
 var update = function () {
     //update 
     if (myAlliance !== "") {
-        $("#player-info").html("<div class='characterPick'><img src='assets/images/Captains/"+myCaptain+".jpg' alt='"+myCaptain+"'><p>"+myCaptain+"</p>");
-        $("#yourHP").html("");
+        $("#player-info").html("<div class='characterPick'><img src='assets/images/Captains/" + myCaptain + ".jpg' alt='" + myCaptain + "'><p>" + myCaptain + "</p></div><div><img src='assets/images/Ships/" + myShip + ".jpg' alt='" + myShip + "'><p>" + myShip + "</p></div><div><img src='assets/images/Logos/" + myAlliance.shortName + "_logo.png' alt='" + myAlliance.shortName + "'><p>" + myAlliance.fullName + "</p></div>");
+        $("#yourHP").html(myAlliance.shieldHP);
     }
     else {
-        //if you pick a side stuff goes here
+        $("#player-info").html("");
     }
     if (opponentAlliance !== "") {
+        $("#opponent-info").html("<div><img src='assets/images/Captains/" + opponentCaptain + ".jpg' alt='" + opponentCaptain + "'><p>" + opponentCaptain + "</p></div><div><img src='assets/images/Ships/" + opponentShip + ".jpg' alt='" + opponentShip + "'><p>" + opponentShip + "</p></div><div><img src='assets/images/Logos/" + opponentAlliance.shortName + "_logo.png' alt='" + opponentAlliance.shortName + "'><p>" + opponentAlliance.fullName + "</p></div>");
+        $("#yourHP").html(opponentAlliance.shieldHP);
+    }
+    else {
         $("#opponent-info").html("");
         $("#theirHP").html("");
     }
-    else {
-        //if you picked opponent it goes here
-    }
     $("#opponentsLeft").html("");
     opponentsLeft.forEach(element => {
-        $("#opponentsLeft").append("<img src='assets/images/Logos/" + element.shortName + "_logo.png' src = '" + element.fullName + "' id='button" + element.shortName + "'>");
+        if(element!=="") {
+            $("#opponentsLeft").append("<img src='assets/images/Logos/" + element.shortName + "_logo.png' src = '" + element.fullName + "' id='button" + element.shortName + "'>");
+        }
     });
 }
 
@@ -76,13 +79,19 @@ var pickCapAndShip = function (which) {
         myShip = myAlliance.ship[randomPick];
     }
     else if (which === "yours") {
-
+        opponentCaptain = opponentAlliance.fighter[randomPick];
+        opponentShip = opponentAlliance.ship[randomPick];
     }
 
 }
 
-var showCapAndShip = function() {
-    $(".choiceBox").html("<h2>Your Captain & Ship</h2><div class='characterPick'><img src='assets/images/Captains/"+myCaptain+".jpg' alt='"+myCaptain+"'><p>"+myCaptain+"</p></div><div class='characterPick'><img src='assets/images/Ships/"+myShip+".jpg' alt='"+myShip+"'><p>"+myShip+"</p></div><button class='proceedButton' id='pickOpponent'>Pick Opponent</button>");
+var showCapAndShip = function (whose) {
+    if(whose==="mine") {
+        $(".choiceBox").html("<h2>Your Captain & Ship</h2><div class='characterPick'><img src='assets/images/Captains/" + myCaptain + ".jpg' alt='" + myCaptain + "'><p>" + myCaptain + "</p></div><div class='characterPick'><img src='assets/images/Ships/" + myShip + ".jpg' alt='" + myShip + "'><p>" + myShip + "</p></div><button class='proceedButton' id='pickOpponent'>Pick Opponent</button>");
+    }
+    else if(whose==="yours") {
+        $(".choiceBox").html("<h2>Their Captain & Ship</h2><div class='characterPick'><img src='assets/images/Captains/" + opponentCaptain + ".jpg' alt='" + opponentCaptain + "'><p>" + opponentCaptain + "</p></div><div class='characterPick'><img src='assets/images/Ships/" + opponentShip + ".jpg' alt='" + opponentShip + "'><p>" + opponentShip + "</p></div><button class='proceedButton' id='fight'>Fight</button>");
+    }
 
     update();
 }
@@ -93,36 +102,59 @@ $(document).ready(function () {
 
     $(document).on("click", ".Im-romulan", function () {
         myAlliance = romulan;
+        opponentsLeft[opponentsLeft.indexOf(romulan)] = "";
         pickCapAndShip("mine");
-        showCapAndShip();
+        showCapAndShip("mine");
     });
     $(document).on("click", ".Im-klingon", function () {
         myAlliance = klingon;
+        opponentsLeft[opponentsLeft.indexOf(klingon)] = "";
         pickCapAndShip("mine");
-        showCapAndShip();
+        showCapAndShip("mine");
     });
     $(document).on("click", ".Im-federation", function () {
         myAlliance = federation;
+        opponentsLeft[opponentsLeft.indexOf(federation)] = "";
         pickCapAndShip("mine");
-        showCapAndShip();
+        showCapAndShip("mine");
     });
     $(document).on("click", ".Im-dominion", function () {
         myAlliance = dominion;
+        opponentsLeft[opponentsLeft.indexOf(dominion)] = "";
         pickCapAndShip("mine");
-        showCapAndShip();
+        showCapAndShip("mine");
     });
     $(document).on("click", "#pickOpponent", function () {
-        console.log("hi");
+        $(".choiceBox").html("<h2 id='insideHeader'>Pick Your Opponent</h2>");
+        opponentsLeft.forEach(element => {
+            if (element !== "") {
+                $(".choiceBox").append("<div class='Theyre-" + element.shortName + "'><img src='assets/images/Logos/" + element.shortName + "_logo.png' src='" + element.fullName + "'><p>" + element.fullName + "</p></div>");
+            }
+        });
     });
-
-
-
-
-
-
-
-
-
-
+    $(document).on("click", ".Theyre-romulan", function () {
+        opponentAlliance = romulan;
+        opponentsLeft[opponentsLeft.indexOf(romulan)] = "";
+        pickCapAndShip("yours");
+        showCapAndShip("yours");
+    });
+    $(document).on("click", ".Theyre-klingon", function () {
+        opponentAlliance = klingon;
+        opponentsLeft[opponentsLeft.indexOf(klingon)] = "";
+        pickCapAndShip("yours");
+        showCapAndShip("yours");
+    });
+    $(document).on("click", ".Theyre-federation", function () {
+        opponentAlliance = federation;
+        opponentsLeft[opponentsLeft.indexOf(federation)] = "";
+        pickCapAndShip("yours");
+        showCapAndShip("yours");
+    });
+    $(document).on("click", ".Theyre-dominion", function () {
+        opponentAlliance = dominion;
+        opponentsLeft[opponentsLeft.indexOf(dominion)] = "";
+        pickCapAndShip("yours");
+        showCapAndShip("yours");
+    });
 
 });
